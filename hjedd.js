125a0b31bd12b9bdf869857ce91b57897bc7a18a
sssxttt <91664383+sssxttt@users.noreply.github.com>
2022-12-30 09:15:02 +0800

    Create hjedd.js

--- hjedd.js
+++ hjedd.js
@@ -1,76 +1,153 @@
 (function () {
+    $('head').append($(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">`));
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+        }
+        return "";
+    }
+
+    let account = {
+        username: '', // 用户名
+        password: '' // 密码
+    }
+
 
     // Your code here...
-    // 引入 CSS
-    $('head').append($(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">`));
-    // 使用 Bootstrap 搭建简洁的界面
-
-
     $(document).ready(function () {
+        let count1 = 0
+        let count2 = 0
+        let dialog1 = setInterval(function () {
+            count1++;
+            if (count1 > 10) {
+                clearInterval(dialog1)
+            }
+            if (document.evaluate('/html/body/div[1]/div[6]/div[1]/div/div[3]/span/button', document).iterateNext()) {
+                document.evaluate('/html/body/div[1]/div[6]/div[1]/div/div[3]/span/button', document).iterateNext().click()
+                clearInterval(dialog1)
+            }
+        }, 200)
+        let dialog2 = setInterval(function () {
+            count2++
+            if (count2 > 10) {
+                clearInterval(dialog2)
+            }
+            if (document.evaluate('/html/body/div/div[6]/div[2]/div/div[3]/span/span', document).iterateNext()) {
+                document.evaluate('/html/body/div/div[6]/div[2]/div/div[3]/span/span', document).iterateNext().click()
+                clearInterval(dialog2)
+            }
+        }, 200)
+
+
+        if (getCookie('NOTLOGIN') == 'NOTLOGIN' || !document.cookie) {
+            console.log('当前未登录...')
+            console.log('开始尝试登陆...')
+
+
+            let loginPollingCount = 0;
+            let loginPolling = setInterval(function () {
+                loginPollingCount++
+                if (loginPollingCount > 10) {
+                    clearInterval(loginPolling)
+                }
+                let loginDiv = document.evaluate('/html/body/div/div[1]/div/div[3]/div/div[1]', document).iterateNext();
+                if (loginDiv) {
+                    console.log('执行登录...')
+                    clearInterval(loginPolling)
+                    loginDiv.click()
+                }
+            }, 200)
+
+
+
+            let loginBoxCount = 0;
+            let loginBoxPolling = setInterval(function () {
+                loginBoxCount++;
+                if (loginBoxCount > 10) {
+                    clearInterval(loginBoxPolling);
+                }
+
+
+
+                let username = document.evaluate('/html/body/div/div[2]/div/div/form/div[1]/div/div/input', document).iterateNext();
+                let password = document.evaluate('/html/body/div/div[2]/div/div/form/div[2]/div/div/input', document).iterateNext();
+
+
+                if (username || password) {
+                    clearInterval(loginBoxPolling);
+                    if (!account) {
+                        console.log('未检测到账号，退出登录');
+                        return;
+                    }
+                    console.log(username,password)
+
+                    username.value = account.username;
+                    password.value = account.password;
+
+                    // 触发输入事件
+                    username.dispatchEvent(new Event('input'));
+                    password.dispatchEvent(new Event('input'));
+
+                    let loginBtn = document.evaluate('/html/body/div/div[2]/div/div/form/div[3]/div/button', document).iterateNext();
+                    loginBtn.click();
+                }
+
+            }, 200)
+
+
+            let msgCount = 0;
+            let msgPolling = setInterval(function () {
+                msgCount++;
+                if (msgCount > 10) {
+                    clearInterval(msgPolling);
+                }
+
+                if (document.evaluate('/html/body/div[2]/div/div[3]/button[1]', document).iterateNext()) {
+                    clearInterval(msgPolling);
+                    document.evaluate('/html/body/div[2]/div/div[3]/button[1]', document).iterateNext().click();
+                    console.log('登陆成功')
+                }
+
+            }, 200)
+
+
+        }
+
+
+        // 注入解析功能
+
 
         var divapp = $(`<button id="ope" class="btn btn-primary" style="position: fixed; z-index:9999;width:100px;height:40px;top:80px;left:1%;font-size:14px;">解析视频</button>`);
 
         $('body').append(divapp);
+        let flag = false;
 
         $('#ope').click(function () {
+            let dataUrl = document.querySelector('div.video-div').getAttribute("data-url");
+            let mp4 = dataUrl.substring(0, dataUrl.lastIndexOf('/')) + '.mp4';
+            let mov = dataUrl.substring(0, dataUrl.lastIndexOf('/')) + '.mov';
 
-
-            if ( $('#button1') ) {
-                $('#button1').remove();
-            }
-            if ( $('#button2') ) {
-                $('#button2').remove();
-            }
-
-            let videoNodes = document.getElementsByClassName("video-div-btn");
-            console.log('当前界面包含：%d 个视频！！！', videoNodes.length);
-            if (videoNodes.length > 0) {
-                for (let i = 0; i < videoNodes.length; i++) {
-                    console.log('开始解析第 %d 个视频', (i + 1));
-                    let videoNodeId = videoNodes[i].id;
-                    let videoNode = document.getElementById(videoNodeId);
-                    let videoPreviewUrl = videoNode.getAttribute('data-url');
-
-                    let mp4Url;
-                    let movUrl;
-                    // 解析视频链接
-                    if (videoPreviewUrl.search(/.m3u8/i)) {
-                        mp4Url = videoPreviewUrl.substr(0, videoPreviewUrl.lastIndexOf('/')) + '.mp4';
-                        movUrl = videoPreviewUrl.substr(0, videoPreviewUrl.lastIndexOf('/')) + '.mov';
-                    }
-
-                    let button1 = document.createElement('button');
-                    button1.id = 'button1';
-                    button1.innerText = 'Go for free MP4!!!';
-                    button1.style = 'width:60px; height:100px;margin: 0 10px;';
-                    button1.addEventListener('click', function () {
-                        window.open(mp4Url);
-                    });
-
-                    let button2 = document.createElement('button');
-                    button2.id = 'button2';
-                    button2.innerText = 'Go for free MOV!!!';
-                    button2.style = 'width:60px; height:100px;';
-
-                    button2.addEventListener('click', function () {
-                        window.open(movUrl);
-                    });
-
-
-
-                    let box = document.getElementsByClassName('action-tyf d-flex justify-content-center mt-2')[0];
-                    box.appendChild(button1);
-                    box.appendChild(button2);
-
-                }
+            if (!flag) {
+                document.querySelector('video.dplayer-video').setAttribute('src', mp4);
+                document.querySelector('span.sell_line2').innerHTML = 'mp4解析中...当播放器中出现的时长与视频预计的时长相等时，就别在按了！'
+                flag = !flag;
             } else {
-                // 消息提示
-                alert('当前页面未找到视频！！！');
-                console.log('当前页面未找到视频！！！');
+                document.querySelector('video.dplayer-video').setAttribute('src', mov);
+                document.querySelector('span.sell_line2').innerHTML = 'mov解析中...当播放器中出现的时长与视频预计的时长相等时，就别在按了！'
+                flag = !flag;
             }
-        });
+
+
+        })
+
+
+
+
+
+
 
     })
-
-
 })();
